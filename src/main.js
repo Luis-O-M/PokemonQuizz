8d11ca0df11b8005f6ddd4a81f11477259a7bf7a
Víctor Sabido <vsabidorubio@gmail.com>
2021-09-30 22:47:40 +0200

    Pokemon init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// import App from './pages/PokemonPage.vue'
+
+import './css/animations.css'
+import './css/styles.css'
 
 createApp(App).mount('#app')
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h1>¿Que Pokemon es?</h1>
+
+        <PokemonPicture :pokemonId="17" :showPokemon="true" />
+        <PokemonOptions />
+  </div>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+    components: {
+        PokemonOptions, PokemonPicture
+    }
+}
+</script>
